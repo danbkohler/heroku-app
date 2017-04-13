63f78e1d77b5566d541a34dfd171282e506f4167
Dan at school <dan.kohler@bellevuecollege.edu>
2017-04-12 17:19:06 -0700

    end of class changes

--- week2-react-jest/src/App.js
+++ week2-react-jest/src/App.js
@@ -3,19 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            nine: '0'
+        }
+    }
+
+    getNine = () => {
+        console.log('state');
+        this.setState({nine: '9'})
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to PROG 272</h2>
+                </div>
+                <p className="App-intro">
+                    Nine: {this.state.nine}
+                </p>
+
+
+                <button className='elf' onClick={this.getNine}>Get Nine</button>
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- week2-react-jest/src/App.test.js
+++ week2-react-jest/src/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('React Jest Suite', function () {
+
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
+
+    it('renders and reads H1 text', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <h2>Welcome to React</h2>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('see if true is true', () => {
+        expect(true).toBe(true);
+    });
 });
