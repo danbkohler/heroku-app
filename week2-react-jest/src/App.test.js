fab72bac81f4c5744613505f7d79cd6a60f4e228
Dan at school <dan.kohler@bellevuecollege.edu>
2017-04-17 14:43:43 -0700

    for Unit Tests with Jest

--- week2-react-jest/src/App.test.js
+++ week2-react-jest/src/App.test.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { shallow } from 'enzyme';
 
-describe('React Jest Suite', function () {
+//React Jest Suite
+//Jest Create React Tests
+describe('jest test', function () {
 
 
     it('renders without crashing', () => {
@@ -20,4 +22,13 @@ describe('React Jest Suite', function () {
     it('see if true is true', () => {
         expect(true).toBe(true);
     });
+
+    //New code
+    it('renders button click message', () => {
+       const wrapper = shallow(<App />);
+       const nineSign = <p className="App-intro">Nine: 9</p>;
+       wrapper.find('button.elf').simulate('click');
+       expect(wrapper.contains(nineSign)).toEqual(true);
+    });
+
 });
