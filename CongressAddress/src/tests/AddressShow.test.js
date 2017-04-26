e2335dacfbb1032e058b4785624013e6d5494d77
Dan at school <dan.kohler@bellevuecollege.edu>
2017-04-26 16:58:11 -0700

    Second push for ReactAddressShow

--- CongressAddress/src/tests/AddressShow.test.js
+++ CongressAddress/src/tests/AddressShow.test.js
@@ -9,14 +9,38 @@ import addresses from '../address-list';
 
 //This file does nothing yet.
 
-/*
-it('renders and displays the default full name', () => {
-    const wrapper = shallow(<AddressShow addressList={addresses} />);
-    //const welcome = <p className="App-intro">firstName: unknown</p>;
-    //const firstName = wrapper.find('p').last().debug();
-    //console.log(firstName);
-    //expect(wrapper.contains(welcome)).toEqual(true);
-    const fullName = <p>Name: unknown unknown </p>;
-    expect(wrapper.contains(fullName)).toEqual(true);
+describe('AddressShow Shallow Suite', function () {
+    var quiet = true;
+
+    const getIndex = function(wrapper, index, talkToMe) {
+        if (!quiet || talkToMe) {
+            const ninep = wrapper.find('div#addressShowRender').childAt(index).debug();
+            console.log('NINEP', ninep);
+        }
+    };
+
+    const defaultFieldTest = (name, index, talkToMe) => {
+        const wrapper = shallow(<AddressShow address={address}  />);
+        const welcome = <p className="App-intro">{name}</p>;
+        getIndex(wrapper, index, talkToMe);
+        expect(wrapper.contains(welcome)).toEqual(true);
+    };
+
+    it('renders and displays the default first name', () => {
+        defaultFieldTest('firstName: unknown', 0);
+    });
+
+
 });
-*/
+
+/*
+ it('renders and displays the default full name', () => {
+ const wrapper = shallow(<AddressShow addressList={addresses} />);
+ //const welcome = <p className="App-intro">firstName: unknown</p>;
+ //const firstName = wrapper.find('p').last().debug();
+ //console.log(firstName);
+ //expect(wrapper.contains(welcome)).toEqual(true);
+ const fullName = <p>Name: unknown unknown </p>;
+ expect(wrapper.contains(fullName)).toEqual(true);
+ });
+ */
